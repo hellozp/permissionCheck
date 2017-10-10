93c0d60da05bf7e8926393611dd49e79a0a90869
zhangp <943611512@qq.com>
2017-10-10 16:44:42 +0800

    解决6.0x系统以上敏感权限检测问题的

---
+++ MyPermissionCheck/app/src/main/java/com/zp/mypermissioncheck/AppContants.java
@@ -0,0 +1,16 @@
+package com.zp.mypermissioncheck;
+
+/**
+ * author : zp
+ * e-mail : zhangping1@hrbb.com.cn
+ * time : 2017/9/28 10:17
+ * desc :
+ * version: 1.0
+ */
+
+class AppContants {
+    /**
+     * 6.0权限检测参数回调标识
+     */
+    public static final int REQUEST_CODE = 101;
+}
